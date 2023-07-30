703b0ee9fc57935bcb3dcedd69f0216b6cd70de9
Kafil <18-38175-2@student.aiub.edu>
2023-07-30 17:47:30 +0600

    ...

--- resources/views/photos/showByCategory.blade.php
+++ resources/views/photos/showByCategory.blade.php
@@ -1,5 +1,9 @@
 @foreach ($photos as $photo)
     <span> {{ $photo->name }} </span>
+    <div>
+        <img src="{{ asset('images/' . $photo->name) }}"
+        height= "200px" width= "200px">
+    </div>
 @endforeach
 
 <p> {{ $category }} </p>
